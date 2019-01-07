f519f58f8b61a04c9ab86bff33054ae1bf2bc6f9
0x6A6F7368 <43834212+0x6A6F7368@users.noreply.github.com>
2019-01-07 16:48:08 +1100

    Tidy up diceware password output

--- diceware.py
+++ diceware.py
@@ -2,7 +2,7 @@ import secrets
 
 def dicenumber():
     diceout = ""
-    for i in range(5):  # 5 dice rolls
+    for i in range(5):    # 5 dice rolls as each wordlist number is identified by 5 numbers
         rolloutput = str(secrets.randbelow(6))    # add result of dice roll to variable
         while rolloutput == "0": # dice cannot roll zero
             rolloutput = str(secrets.randbelow(6))
@@ -10,10 +10,13 @@ def dicenumber():
     return diceout
 
 def diceware(x):
-    wordlist = open('diceware.wordlist.asc', 'r') # http://world.std.com/~reinhold/diceware.wordlist.asc
+    wordlist = open('diceware.wordlist.asc', 'r')
     for line in wordlist:
         if line.startswith(x):
             wordlist.close()
-            return line
+            line = line.strip(x)    # remove wordlist numbering
+            return line.strip()    # remove tabs and newlines
 
-for i in range(5): print(diceware(dicenumber())),    # increase number for longer passphrases
+passphrase = []
+for i in range(6): passphrase.append(diceware(dicenumber()))    # increase range number for longer passphrases
+print('-'.join(passphrase))    # prints final passphrase
